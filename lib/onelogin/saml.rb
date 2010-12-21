af0a0f4c13c7d6f1ed350018f9333976bfa94d7d
Hiroyuki Nakamura <hello@hryk.info>
2010-12-21 21:50:30 +0900

    added require logoutrequest

--- lib/onelogin/saml.rb
+++ lib/onelogin/saml.rb
@@ -1,4 +1,5 @@
 require 'onelogin/saml/authrequest'
+require 'onelogin/saml/logoutrequest'
 require 'onelogin/saml/response'
 require 'onelogin/saml/settings'
 require 'onelogin/saml/entity_descriptor'
