e61cff2b0716b3e2381541ca13f1a2ed8d5e1d86
zoodor <zoodor@hotmail>
2012-08-07 17:11:16 +0100

    Updated to remove time-zone component from the IssueInstant time (this is to conform with SAMLCore-2.0, section 1.3.3).

--- lib/onelogin/ruby-saml/authrequest.rb
+++ lib/onelogin/ruby-saml/authrequest.rb
@@ -32,7 +32,7 @@ module Onelogin
 
       def create_authentication_xml_doc(settings)
         uuid = "_" + UUID.new.generate
-        time = Time.now.utc.strftime("%Y-%m-%dT%H:%M:%SZ")
+        time = Time.now.utc.strftime("%Y-%m-%dT%H:%M:%S")
         # Create AuthnRequest root element using REXML 
         request_doc = REXML::Document.new
 
