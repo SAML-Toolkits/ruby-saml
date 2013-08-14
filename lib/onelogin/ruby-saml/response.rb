8342f49c241baff70987679f270742b7b20d9ada
Christian Pedersen <christian@onelogin.com>
2013-08-14 13:04:40 -0700

    more diagnostics

--- lib/onelogin/ruby-saml/response.rb
+++ lib/onelogin/ruby-saml/response.rb
@@ -24,7 +24,7 @@ module OneLogin
         raise ArgumentError.new("Response cannot be nil") if response.nil?
         @options  = options
         @response = (response =~ /^</) ? response : Base64.decode64(response)
-        @document = XMLSecurity::SignedDocument.new(@response)
+        @document = XMLSecurity::SignedDocument.new(@response, @errors)
       end
 
       def is_valid?
@@ -144,9 +144,9 @@ module OneLogin
           @xml = Nokogiri::XML(self.document.to_s)
         end
         if soft
-          @schema.validate(@xml).map{ return false }
+          @schema.validate(@xml).map{ @errors << "Schema validation failed"; return false }
         else
-          @schema.validate(@xml).map{ |error| validation_error("#{error.message}\n\n#{@xml.to_s}") }
+          @schema.validate(@xml).map{ |error| @errors << "#{error.message}\n\n#{@xml.to_s}"; validation_error("#{error.message}\n\n#{@xml.to_s}") }
         end
       end
 
