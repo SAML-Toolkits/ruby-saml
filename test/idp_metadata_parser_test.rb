f878afedf95514a1bea63f65810423b0144b123e
Geoffrey Hichborn <geoff@socialcast.com>
2014-03-18 13:02:58 -0700

    Add support for extracting IdP details from metadata xml

---
+++ test/idp_metadata_parser_test.rb
@@ -0,0 +1,17 @@
+require File.expand_path(File.join(File.dirname(__FILE__), "test_helper"))
+
+class IdpMetadataParserTest < Test::Unit::TestCase
+
+  context "parsing an IdP descriptor file" do
+    should "extract settings details from xml" do
+      idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
+
+      settings = idp_metadata_parser.parse(idp_metadata)
+
+      assert_equal "https://example.hello.com/access/saml/login", settings.idp_sso_target_url
+      assert_equal "F1:3C:6B:80:90:5A:03:0E:6C:91:3E:5D:15:FA:DD:B0:16:45:48:72", settings.idp_cert_fingerprint
+      assert_equal "https://example.hello.com/access/saml/logout", settings.idp_slo_target_url
+    end
+  end
+
+end
