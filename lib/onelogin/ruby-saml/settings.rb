3789b228f98c2cb3fba6e9f7823e5735377d1a5d
Dan McFadden <d3mcfadden@gmail.com>
2014-05-01 13:15:01 -0400

    Setting support for assertion_consumer_service & assertion_consumer_logout_service

--- lib/onelogin/ruby-saml/settings.rb
+++ lib/onelogin/ruby-saml/settings.rb
@@ -9,12 +9,14 @@ module OneLogin
         end
       end
       attr_accessor :assertion_consumer_service_url, :issuer, :sp_name_qualifier
+      attr_accessor :assertion_consumer_service_binding
       attr_accessor :idp_sso_target_url, :idp_cert_fingerprint, :idp_cert, :name_identifier_format
       attr_accessor :authn_context
       attr_accessor :idp_slo_target_url
       attr_accessor :name_identifier_value
       attr_accessor :sessionindex
       attr_accessor :assertion_consumer_logout_service_url
+      attr_accessor :assertion_consumer_logout_service_binding
       attr_accessor :compress_request
       attr_accessor :double_quote_xml_attribute_values
       attr_accessor :passive
@@ -23,7 +25,15 @@ module OneLogin
 
       private
 
-      DEFAULTS = {:compress_request => true, :sign_request => false, :double_quote_xml_attribute_values => false, :digest_method => "SHA1", :signature_method => "SHA1"}
+      DEFAULTS = {
+        :assertion_consumer_service_binding        => "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST",
+        :assertion_consumer_logout_service_binding => "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect",
+        :compress_request                          => true,
+        :sign_request                              => false,
+        :double_quote_xml_attribute_values         => false,
+        :digest_method                             => "SHA1",
+        :signature_method                          => "SHA1"
+      }
     end
   end
 end
