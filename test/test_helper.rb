a95f86ae9de4fa847bb8787d13a1a563a3299764
Geoffrey Hichborn <geoff@socialcast.com>
2014-07-14 13:20:42 -0700

    Fixed misspelling

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -46,7 +46,7 @@ class Test::Unit::TestCase
   end
 
   def ampersands_response
-    @ampersands_resposne ||= File.read(File.join(File.dirname(__FILE__), 'responses', 'response_with_ampersands.xml.base64'))
+    @ampersands_response ||= File.read(File.join(File.dirname(__FILE__), 'responses', 'response_with_ampersands.xml.base64'))
   end
 
   def response_document_6
