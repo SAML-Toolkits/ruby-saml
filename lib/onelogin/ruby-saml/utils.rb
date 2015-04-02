cc9de953491322700b3e4f5e00b5e1797067ff0d
Phil Cohen <github@phlippers.net>
2015-04-01 18:17:37 -0700

    ensure IdP private key is formatted properly:
    * this ensures that the private key will be formatted in a
      manner consistent with the certificate

--- lib/onelogin/ruby-saml/utils.rb
+++ lib/onelogin/ruby-saml/utils.rb
@@ -13,29 +13,22 @@ module OneLogin
         "-----BEGIN CERTIFICATE-----\n#{cert}\n-----END CERTIFICATE-----"
       end
 
-      def self.format_private_key(key, heads=true)
-        key = key.delete("\n").delete("\r").delete("\x0D")
-        if key
-          if key.index('-----BEGIN PRIVATE KEY-----') != nil
-            key = key.gsub('-----BEGIN PRIVATE KEY-----', '')
-            key = key.gsub('-----END PRIVATE KEY-----', '')
-            key = key.gsub(' ', '')
-            if heads
-              key = key.scan(/.{1,64}/).join("\n")+"\n"
-              key = "-----BEGIN PRIVATE KEY-----\n" + key + "-----END PRIVATE KEY-----\n"
-            end
-          else
-            key = key.gsub('-----BEGIN RSA PRIVATE KEY-----', '')
-            key = key.gsub('-----END RSA PRIVATE KEY-----', '')
-            key = key.gsub(' ', '')
-            if heads
-              key = key.scan(/.{1,64}/).join("\n")+"\n"
-              key = "-----BEGIN RSA PRIVATE KEY-----\n" + key + "-----END RSA PRIVATE KEY-----\n"
-            end
-          end
-        end
-      end
+      def self.format_private_key(key)
+        # don't try to format an encoded certificate
+        return key if key.match(/\x0d/)
 
+        # is this an rsa key?
+        rsa_key = key.match("RSA PRIVATE KEY")
+
+        key = key.gsub(/\-{5}\s?(BEGIN|END)( RSA)? PRIVATE KEY\s?\-{5}/, "")
+        key = key.gsub(/[\n\r\s]/, "")
+        key = key.scan(/.{1,64}/)
+        key = key.join("\n")
+
+        key_label = rsa_key ? "RSA PRIVATE KEY" : "PRIVATE KEY"
+
+        "-----BEGIN #{key_label}-----\n#{key}\n-----END #{key_label}-----"
+      end
     end
   end
 end
--- test/utils_test.rb
+++ test/utils_test.rb
@@ -25,4 +25,54 @@ class UtilsTest < Minitest::Test
       assert_equal valid_certificate, OneLogin::RubySaml::Utils.format_cert(invalid_certificate3)
     end
   end
+
+  describe ".format_private_key" do
+    let(:valid_private_key) do
+      read_certificate("valid_private_key")
+    end
+
+    it "returns the private key when it is valid" do
+      assert_equal valid_private_key, OneLogin::RubySaml::Utils.format_private_key(valid_private_key)
+    end
+
+    it "reformats the private key when there are spaces and no line breaks" do
+      invalid_private_key1 = read_certificate("invalid_private_key1")
+      assert_equal valid_private_key, OneLogin::RubySaml::Utils.format_private_key(invalid_private_key1)
+    end
+
+    it "reformats the private key when there are spaces and no headers" do
+      invalid_private_key2 = read_certificate("invalid_private_key2")
+      assert_equal valid_private_key, OneLogin::RubySaml::Utils.format_private_key(invalid_private_key2)
+    end
+
+    it "reformats the private key when there line breaks and no headers" do
+      invalid_private_key3 = read_certificate("invalid_private_key3")
+      assert_equal valid_private_key, OneLogin::RubySaml::Utils.format_private_key(invalid_private_key3)
+    end
+
+    describe "an RSA public key" do
+      let(:valid_rsa_private_key) do
+        read_certificate("valid_rsa_private_key")
+      end
+
+      it "returns the private key when it is valid" do
+        assert_equal valid_rsa_private_key, OneLogin::RubySaml::Utils.format_private_key(valid_rsa_private_key)
+      end
+
+      it "reformats the private key when there are spaces and no line breaks" do
+        invalid_rsa_private_key1 = read_certificate("invalid_rsa_private_key1")
+        assert_equal valid_rsa_private_key, OneLogin::RubySaml::Utils.format_private_key(invalid_rsa_private_key1)
+      end
+
+      it "reformats the private key when there are spaces and no headers" do
+        invalid_rsa_private_key2 = read_certificate("invalid_rsa_private_key2")
+        assert_equal valid_private_key, OneLogin::RubySaml::Utils.format_private_key(invalid_rsa_private_key2)
+      end
+
+      it "reformats the private key when there line breaks and no headers" do
+        invalid_rsa_private_key3 = read_certificate("invalid_rsa_private_key3")
+        assert_equal valid_private_key, OneLogin::RubySaml::Utils.format_private_key(invalid_rsa_private_key3)
+      end
+    end
+  end
 end
