866703094c114830a59664b5a0d363090a51f27d
Sixto Martin <pitbulk@gmail.com>
2015-04-29 04:30:56 +0200

    Some features from the PR #197 (PR splitted) * Comment the code * Remove spaces and format some lines * Remove unnecesary errors method

--- lib/onelogin/ruby-saml/idp_metadata_parser.rb
+++ lib/onelogin/ruby-saml/idp_metadata_parser.rb
@@ -7,10 +7,13 @@ require "net/https"
 require "rexml/document"
 require "rexml/xpath"
 
+# Only supports SAML 2.0
 module OneLogin
   module RubySaml
     include REXML
 
+    # Auxiliary class to retrieve and parse the Identity Provider Metadata
+    #
     class IdpMetadataParser
 
       METADATA = "urn:oasis:names:tc:SAML:2.0:metadata"
@@ -18,11 +21,18 @@ module OneLogin
 
       attr_reader :document
 
+      # Parse the Identity Provider metadata and update the settings with the IdP values
+      # @param url [String] Url where the XML of the Identity Provider Metadata is published.
+      # @param validate_cert [Boolean] If true and the URL is HTTPs, the cert of the domain is checked.
+      #
       def parse_remote(url, validate_cert = true)
         idp_metadata = get_idp_metadata(url, validate_cert)
         parse(idp_metadata)
       end
 
+      # Parse the Identity Provider metadata and update the settings with the IdP values
+      # @param idp_metadata [String] 
+      #
       def parse(idp_metadata)
         @document = REXML::Document.new(idp_metadata)
 
@@ -37,8 +47,11 @@ module OneLogin
 
       private
 
-      # Retrieve the remote IdP metadata from the URL or a cached copy
-      # # returns a REXML document of the metadata
+      # Retrieve the remote IdP metadata from the URL or a cached copy.
+      # @param url [String] Url where the XML of the Identity Provider Metadata is published.
+      # @param validate_cert [Boolean] If true and the URL is HTTPs, the cert of the domain is checked.
+      # @return [REXML::document] Parsed XML IdP metadata
+      #
       def get_idp_metadata(url, validate_cert)
         uri = URI.parse(url)
         if uri.scheme == "http"
@@ -66,33 +79,65 @@ module OneLogin
         meta_text
       end
 
+      # @return [String|nil] IdP Entity ID value if exists
+      #
       def idp_entity_id
-        node = REXML::XPath.first(document, "/md:EntityDescriptor/@entityID", { "md" => METADATA })
+        node = REXML::XPath.first(
+          document,
+          "/md:EntityDescriptor/@entityID",
+          { "md" => METADATA }
+        )
         node.value if node
       end
 
+      # @return [String|nil] IdP Name ID Format value if exists
+      #
       def idp_name_id_format
-        node = REXML::XPath.first(document, "/md:EntityDescriptor/md:IDPSSODescriptor/md:NameIDFormat", { "md" => METADATA })
+        node = REXML::XPath.first(
+          document,
+          "/md:EntityDescriptor/md:IDPSSODescriptor/md:NameIDFormat",
+          { "md" => METADATA }
+        )
         node.text if node
       end
 
+      # @return [String|nil] SingleSignOnService endpoint if exists
+      #
       def single_signon_service_url
-        node = REXML::XPath.first(document, "/md:EntityDescriptor/md:IDPSSODescriptor/md:SingleSignOnService/@Location", { "md" => METADATA })
+        node = REXML::XPath.first(
+          document,
+          "/md:EntityDescriptor/md:IDPSSODescriptor/md:SingleSignOnService/@Location",
+          { "md" => METADATA }
+        )
         node.value if node
       end
 
+      # @return [String|nil] SingleLogoutService endpoint if exists
+      #
       def single_logout_service_url
-        node = REXML::XPath.first(document, "/md:EntityDescriptor/md:IDPSSODescriptor/md:SingleLogoutService/@Location", { "md" => METADATA })
+        node = REXML::XPath.first(
+          document,
+          "/md:EntityDescriptor/md:IDPSSODescriptor/md:SingleLogoutService/@Location",
+          { "md" => METADATA }
+        )
         node.value if node
       end
 
+      # @return [String|nil] X509Certificate if exists
+      #
       def certificate
         @certificate ||= begin
-          node = REXML::XPath.first(document, "/md:EntityDescriptor/md:IDPSSODescriptor/md:KeyDescriptor[@use='signing']/ds:KeyInfo/ds:X509Data/ds:X509Certificate", { "md" => METADATA, "ds" => DSIG })
+          node = REXML::XPath.first(
+            document,
+            "/md:EntityDescriptor/md:IDPSSODescriptor/md:KeyDescriptor[@use='signing']/ds:KeyInfo/ds:X509Data/ds:X509Certificate",
+            { "md" => METADATA, "ds" => DSIG }
+          )
           Base64.decode64(node.text) if node
         end
       end
 
+      # @return [String|nil] the SHA-1 fingerpint of the X509Certificate if it exists
+      #
       def fingerprint
         @fingerprint ||= begin
           if certificate
