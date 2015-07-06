26c36b9fa2e11a1ebe30685203d64b6162bd59be
John Knight <bug@knightlabs.com>
2015-07-06 08:53:02 -0400

    Handle enveloped signatures – see http://www.di-mgt.com.au/xmldsig2.html

--- Gemfile
+++ Gemfile
@@ -4,3 +4,6 @@
 source 'https://rubygems.org'
 
 gemspec
+
+gem 'json'
+gem 'timecop'
--- lib/xml_security.rb
+++ lib/xml_security.rb
@@ -269,7 +269,8 @@ module XMLSecurity
       REXML::XPath.each(@sig_element, "//ds:Reference", {"ds"=>DSIG}) do |ref|
         uri = ref.attributes.get_attribute("URI").value
 
-        hashed_element = document.at_xpath("//*[@ID=$uri]", nil, { 'uri' => uri[1..-1] })
+        # Handle enveloped signatures, not just enveloping signatures: http://www.di-mgt.com.au/xmldsig2.html
+        hashed_element = uri.empty? ? @working_copy : document.at_xpath("//*[@ID=$uri]", nil, { 'uri' => uri[1..-1] })
         canon_algorithm = canon_algorithm REXML::XPath.first(
           ref,
           '//ds:CanonicalizationMethod',
