48cc5b92e1dde8637a013dfd48cdf8ceadd499b2
Sixto Martin <sixto.martin.garcia@workato.com>
2025-03-12 20:40:08 +0100

    Fix version file

--- lib/onelogin/ruby-saml/version.rb
+++ lib/onelogin/ruby-saml/version.rb
@@ -1,13 +1,5 @@
 module OneLogin
   module RubySaml
-<<<<<<< Updated upstream
-    VERSION = '1.12.3'
-=======
-<<<<<<< Updated upstream
-    VERSION = '1.12.0'
-=======
     VERSION = '1.12.4'
->>>>>>> Stashed changes
->>>>>>> Stashed changes
   end
 end
