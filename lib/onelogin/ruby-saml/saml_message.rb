779b7888cff0d511df5bb56bed31d2269fb9f15f
Sixto Martin <sixto.martin.garcia@workato.com>
2025-11-23 11:36:24 +0100

    Revert use of match?. It was introduced on Ruby 2.4 and we keep compatibility with old versions

--- lib/onelogin/ruby-saml/saml_message.rb
+++ lib/onelogin/ruby-saml/saml_message.rb
@@ -136,7 +136,7 @@ module OneLogin
       # @return [true, false] whether or not the string is base64 encoded
       #
       def base64_encoded?(string)
-        string.gsub(/\s|\\r|\\n/, '').match?(BASE64_FORMAT)
+        !!string.gsub(/[\r\n]|\\r|\\n|\s/, "").match(BASE64_FORMAT)
       end
 
       # Attempt inflating a string, if it fails, return the original string.
